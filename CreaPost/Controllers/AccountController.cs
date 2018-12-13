c500f8f843b1d1d227ad7d0a99c24c2af10de6d6
Dawid Wiecheć <wiechec.dawid@gmail.com>
2018-12-13 01:14:16 +0100

    Login added

--- CreaPost/Controllers/AccountController.cs
+++ CreaPost/Controllers/AccountController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CreaPost.Models;
+using CreaPost.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,16 +13,41 @@ namespace CreaPost.Controllers
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
+        [HttpGet]
         public IActionResult Login()
         {
             if(this.User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Create", "Home");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(AccountViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email,
+                    model.Password,
+                    model.RememberMe,
+                    false);
+
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    else
+                        RedirectToAction("Create", "Home");
+                }
+
+                ModelState.AddModelError("", "Failed to Log in");
             }
             return View();
         }
