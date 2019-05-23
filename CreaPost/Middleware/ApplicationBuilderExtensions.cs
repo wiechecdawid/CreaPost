f13b00a1ffdb0d284fc72fc372d734595a789760
Dawid Wiechec <wiechec.dawid@gmail.com>
2019-05-23 19:43:39 +0200

    Bootstrap from node_modules served

--- CreaPost/Middleware/ApplicationBuilderExtensions.cs
+++ CreaPost/Middleware/ApplicationBuilderExtensions.cs
@@ -10,10 +10,12 @@ namespace Microsoft.AspNetCore.Builder
         {
             var path = Path.Combine(root, "node_modules");
 
-            var options = new StaticFileOptions();
-            options.RequestPath = "/node_modules";
 
             PhysicalFileProvider fileProider = new PhysicalFileProvider(path);
+
+            var options = new StaticFileOptions();
+            options.RequestPath = "/node_modules";
+            options.FileProvider = fileProider;
 
             app.UseStaticFiles(options);
 
